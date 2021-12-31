c71d4e9110deea7863c9dbc360b4a802f3c87691
Kiran G <mail@kirang.me>
2021-12-31 19:50:03 +0530

    cmit

---
+++ task3/resume.js
@@ -0,0 +1,96 @@
+{
+  "basics": {
+    "name": "Kiran G",
+    "picture": "https://kirang.me",
+    "email": "mail@kirang.me",
+    "phone": "+917676812288",
+    "degree": "Computer Science & Engineering",
+    "website": "https://kirang.me",
+    "summary": "Full-Stack Web Developer.",
+    "location": {
+      "address": "BRINDAVAN, Vidyanagar, Hassan",
+      "postalCode": "573201",
+      "city": "Hassan",
+      "state": "Karnataka",
+      "countryCode": "IN"
+    },
+    "profiles": [
+      {
+        "network": "github",
+        "username": "notkiran",
+        "url": "https://github.com/notkiran"
+      },
+      {
+        "network": "linkedin",
+        "username": "-kirang",
+        "url": "https://www.linkedin.com/in/-kirang/"
+      },
+      {
+        "network": "twitter",
+        "username": "_kirang",
+        "url": "https://twitter.com/_kirang"
+      }
+    ]
+  },
+  "work": [
+    {
+      "company": "Company Name",
+      "position": "Web Developer",
+      "website": "The URL for the employer's website",
+      "startDate": "01/01/2021",
+      "summary": "Work with a team of Web Developers & contribute to the company projects."
+    }
+  ],
+  "education": [
+    {
+      "institution": "East West Institute of Technology",
+      "area": "Computer Science",
+      "startDate": "Aug/2016",
+      "endDate": "Sept/2021",
+      "gpa": "6"
+    }
+  ],
+  "skills": [
+    {
+      "name": "Programming Languages",
+      "level": "Intermediate",
+      "keywords": [
+        "HTML",
+        "CSS",
+        "JAVA",
+        "JavaScript",
+        "REACT",
+        "NODE",
+        "EXPRESS"
+      ]
+    }
+  ],
+  "languages": [
+    {
+      "language": "English",
+      "fluency": "Fluent"
+    },
+    {
+      "language": "Hindi",
+      "fluency": "Fluent"
+    },
+    {
+      "language": "Kkannada",
+      "fluency": "Fluent"
+    }
+  ],
+  "interests": [
+    {
+      "name": "Sports",
+      "keywords": ["Cricket", "Football", "Table Tennis"]
+    },
+    {
+      "name": "Gaming",
+      "keywords": ["Overwatch", "Battlefield", "Valorant"]
+    },
+    {
+      "name": "Movies",
+      "keywords": ["Thrillers", "Horror", "Scifi"]
+    }
+  ]
+}
